94c595f06d5984d0cb7389e8969208b76dd68419
naderganayem-74 <nader@nader.expert>
2021-04-08 10:33:08 +0000

    Removed test lines

--- script-1.bash
+++ script-1.bash
@@ -4,29 +4,6 @@
 echo -n "Enter a directory to delete: "
 #echo -n "Enter a number: "
 read VAR
-echo $VAR1 | od -x
-VAR1="$(echo $VAR | sed 's/\n//g')"
-VAR1=${VAR1%$'\n'}   
-VAR1=$(ls d)
-
-if [ -z "$VAR1" ] 
-then 
-  echo $VAR1 | od -x
-  echo "VAR1 is not empty"
-else
-  echo "Var is empty; exiting!"
-  exit 1
-fi
-
-
-
-
-
-
-
-
-
-
 
 if [[ $VAR -gt 10 ]]
 then 
